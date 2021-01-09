6858abe3d8ad9a9463202e4237e7e0ad089990e2
Engozdev <denacua5@gmail.com>
2021-01-09 20:15:37 +0300

    All condition are shown on the task form

--- task_show.py
+++ task_show.py
@@ -3,22 +3,27 @@ import sys
 import pygame
 
 
-def show_task():
+def show_task(image_path, question, correct_answer):
     pygame.init()
     size = width, height = 1280, 960
     screen = pygame.display.set_mode(size)
     fon = pygame.transform.scale(load_image('Terminal.jpg'), (width, height))
     screen.blit(fon, (0, 0))
 
+    pygame.font.init()
+    font = pygame.font.SysFont('Arial', 30)
+    quest = font.render(question, False, (255, 255, 255))
+    screen.blit(quest, (250, 230))
+
     all_sprites = pygame.sprite.Group()
     task = pygame.sprite.Sprite()
-    task.image = load_image("nepal.png")
-    task.rect = 800, 150
-
+    task.image = load_image(image_path)
+    task.rect = 750, 150
     all_sprites.add(task)
+
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     ans = ''
-
+    corr_flag = False
     clock = pygame.time.Clock()
     running = True
     while running:
@@ -27,8 +32,32 @@ def show_task():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
-                ans += alphabet[event.key - 97]
+                # print(event.key)
+                if event.key == 13:
+                    if ans == correct_answer:
+                        # ans = ''
+                        pygame.draw.circle(screen, pygame.Color('green'), (1000, 700), 100)
+                        corr_flag = True
+                    else:
+                        corr_flag = False
+                        pygame.draw.circle(screen, pygame.Color('red'), (1000, 700), 100)
+                elif event.key == 8:
+                    ans = ans[:-1]
+                    screen.blit(fon, (0, 0))
+                    all_sprites.draw(screen)
+                    if corr_flag:
+                        pygame.draw.circle(screen, pygame.Color('green'), (1000, 700), 100)
+                    else:
+                        pygame.draw.circle(screen, pygame.Color('red'), (1000, 700), 100)
+                    screen.blit(quest, (250, 230))
+                else:
+                    try:
+                        ans += alphabet[event.key - 97]
+                    except Exception:
+                        pass
         all_sprites.draw(screen)
+        display_ans = font.render(ans, False, (255, 255, 255))
+        screen.blit(display_ans, (300, 650))
         pygame.display.flip()
     pygame.quit()
 
@@ -50,4 +79,4 @@ def load_image(name, colorkey=None):
     return image
 
 
-show_task()
+show_task("nepal.png", 'Which country has this flag?', 'nepal')
